4670927d33f67839a18999ebe1177cfa56be66be
jul-png <70212985+jul-png@users.noreply.github.com>
2020-12-10 08:41:44 -0700

    added instruction view pray for us

---
+++ SpaceInvader/View/InstructionView.swift
@@ -0,0 +1,20 @@
+//
+//  InstructionView.swift
+//  SpaceInvader
+//
+//  Created by Armstrong, Julian on 12/10/20.
+//
+
+import SwiftUI
+
+struct InstructionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InstructionView_Previews: PreviewProvider {
+    static var previews: some View {
+        InstructionView()
+    }
+}
