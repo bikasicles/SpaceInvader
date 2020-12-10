a9853afa215a3ede2d94484dad409bbc071b1b58
jul-png <70212985+jul-png@users.noreply.github.com>
2020-12-10 08:35:30 -0700

    I did some shit; added sk stuff

---
+++ SpaceInvader/View/StartGameScene.swift
@@ -0,0 +1,16 @@
+//
+//  StartGameScene.swift
+//  SpaceInvader
+//
+//  Created by Armstrong, Julian on 12/10/20.
+//
+
+import UIKit
+import SpriteKit
+class StartGameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+        NSLog("We have loaded the start screen")
+    }
+}
