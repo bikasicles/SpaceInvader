5117814b8c1554c5c7939e03bcda78b73d4489cf
Prus0506 <prestochango1010@gmail.com>
2020-12-10 08:12:21 -0700

    created instructionView

---
+++ SpaceInvader/View/InstructionView.swift
@@ -0,0 +1,20 @@
+//
+//  InstructionView.swift
+//  SpaceInvader
+//
+//  Created by Russell, Preston on 12/10/20.
+//
+
+import SwiftUI
+
+struct InstructionView: View {
+    var body: some View {
+        Text("How to Play!")
+    }
+}
+
+struct InstructionView_Previews: PreviewProvider {
+    static var previews: some View {
+        InstructionView()
+    }
+}
